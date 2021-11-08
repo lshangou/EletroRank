00a4b85d7bbbb9c6953f462ef484603d98db7941
lshangou <lucasmenezesba@gmail.com>
2021-11-08 09:47:19 -0300

    Adicionado axios para requests, view para cadastro de produtos, opção para ler body no servidotr

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const helmet = require('helmet')
 const path = require('path')
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
 const app1 = express() // Compliant
 app1.use(helmet.hidePoweredBy())
 const port = 3000
+
+app1.use(bodyParser.json()) // for parsing application/json
+app1.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app1.set('view engine', 'ejs');
 app1.set('views', path.join(__dirname, '/views'));
@@ -23,11 +29,22 @@ app1.get('/', (req, res) => {
   res.render('index')
 })
 
+app1.get('/product-form', (req, res) => {
+  res.render('cadastro-produtos')
+})
+
 app1.get('/product-list', (req, res) => {
   const products = Product.find({}, function(err, docs) {
     console.log(docs);
     res.render('produtos', {products: docs})
   });
+})
+
+app1.post('/cadastrar-produto', function (req, res, next) {
+  console.log(req.body)
+  const newProduct = new Product({nome: req.body.nome, preco: req.body.preco, tipo: req.body.tipo, descricao: req.body.descricao})
+  newProduct.save()
+  res.json(req.body)
 })
 
 
